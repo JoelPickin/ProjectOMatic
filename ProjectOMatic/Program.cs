2ceedbafb2d7a87e9cdce1634a8a6cdf303f8a88
Joel Pickin <joelpickin@outlook.com>
2022-07-27 15:50:55 +0100

    Changed back

--- ProjectOMatic/Program.cs
+++ ProjectOMatic/Program.cs
@@ -28,13 +28,13 @@ var app = builder.Build();
 
 app.UseAuthentication();
 
-//// Configure the HTTP request pipeline.
-//if (!app.Environment.IsDevelopment())
-//{
-//    app.UseExceptionHandler("/Error");
-//    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-//    app.UseHsts();
-//}
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
 
 app.UseHttpsRedirection();
 
